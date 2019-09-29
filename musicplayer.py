4a85ed4219ce24503062b44baa6d7c1aa3c583c1
harshapps <harshapps1@gmail.com>
2019-09-29 01:15:18 -0500

    Adding socket support and bug fix for next and prev song

--- musicplayer.py
+++ musicplayer.py
@@ -4,11 +4,42 @@ from tkinter.filedialog import askdirectory
 import pygame
 from mutagen.id3 import ID3
 from tkinter import *
+import time
+import socket
+# from thread import *
+from threading import Thread
 
 
+def accept_conn():
+    while True:
+        global client_socket, client_address
+        client_socket, client_address = server_socket.accept()  # Socket accepts any connection request
+        thread2 = Thread(target=receivedData)
+        thread2.start()
+        thread2.join()
+        # start_new_thread(clientthread,(client_socket,client_address),)     #Forking a new thread for the newly connected client
+
+
+def receivedData():
+    global client_socket, client_address
+    print("In Receive")
+    while True:
+        msg = client_socket.recv(1024).decode("utf-8")
+        print(msg)
+        if str(msg).endswith("change") == True:
+            playpausesong()
+        if str(msg).endswith("next") == True:
+            nextsong()
+        if str(msg).endswith("previous") == True:
+            prevsong()
+
+
+client_socket = ""
+client_address = ""
 root = Tk()
 root.minsize(350, 350)
 playflag = False
+volume = ''
 
 listofsongs = []
 realnames = []
@@ -16,27 +47,27 @@ realnames = []
 v = StringVar()
 songlabel = Label(root, textvariable=v, width=35)
 
+no_of_musicfiles = 0
 
 def directorychooser():
-
     directory = askdirectory()
     os.chdir(directory)
 
     for files in os.listdir(directory):
         if files.endswith(".mp3"):
-
             realdir = os.path.realpath(files)
             audio = ID3(realdir)
             realnames.append(audio['TIT2'].text[0])
 
             listofsongs.append(files)
 
+    no_of_musicfiles = len(listofsongs)
     pygame.mixer.init()
     pygame.mixer.music.load(listofsongs[0])
     # pygame.mixer.music.play()
 
 
-index = 1
+index = 0
 directorychooser()
 pygame.mixer.music.load(listofsongs[index])
 pygame.mixer.music.play()
@@ -51,21 +82,28 @@ def updatelabel():
 
 def nextsong(event):
     global index
+    global no_of_musicfiles
+
     index += 1
-    pygame.mixer.music.load(listofsongs[index])
-    pygame.mixer.music.play()
-    updatelabel()
-
-
-def prevsong(event):
-    global index
-    index -= 1
-    # print(index)
-    print(len(listofsongs))
-    if abs(index) <= len(listofsongs[index]):
+    try:
         pygame.mixer.music.load(listofsongs[index])
         pygame.mixer.music.play()
         updatelabel()
+    except:
+        print("No next song available")
+
+def prevsong(event):
+    global index
+    global no_of_musicfiles
+    index -= 1
+
+    # if abs(index) <= no_of_musicfiles:
+    try:
+        pygame.mixer.music.load(listofsongs[index])
+        pygame.mixer.music.play()
+        updatelabel()
+    except:
+        print("No previous song available")
 
 
 def stopsong(event):
@@ -74,7 +112,7 @@ def stopsong(event):
     # return songname
 
 
-def playpausesong(event):
+def playpausesong():
     global playflag
     if playflag:
         pygame.mixer.music.unpause()
@@ -95,8 +133,42 @@ def playsong(event):
     global index
     pygame.mixer.music.load(listofsongs[index])
     pygame.mixer.music.play()
+    # time.sleep(10)
+    # pygame.mixer.music.set_volume(pygame.mixer.music.get_volume() + 0.1)
     v.set("")
     # return songname
+
+
+def decreasevolume(event):
+    global volume
+    pygame.mixer.music.pause()
+    volume = pygame.mixer.music.get_volume()
+    print('volume current', volume)
+    pygame.mixer.music.set_volume(volume - 0.1)
+    pygame.mixer.music.unpause()
+    print('volume is', volume)
+    v.set("")
+
+
+def increasevolume(event):
+    global volume
+    print('volume current', volume)
+    pygame.mixer.music.pause()
+    volume = pygame.mixer.music.get_volume()
+    pygame.mixer.music.set_volume(volume + 0.1)
+    pygame.mixer.music.unpause()
+    print('volume is', volume)
+    v.set("")
+
+    # for volume_index in range(9):
+    #     print('volume is ', volume)
+    #     pygame.mixer.music.load(listofsongs[index])
+    #     time.sleep(10)
+    #     pygame.mixer.music.set_volume(pygame.mixer.music.get_volume() + 0.1)
+    #     volume = pygame.mixer.music.get_volume()
+    #     pygame.mixer.music.play()
+    #     volume_index = volume_index+1
+    # v.set("")
 
 
 label = Label(root, text='Music Player')
@@ -124,18 +196,32 @@ previousbutton.pack()
 stopbutton = Button(root, text='Stop Music')
 stopbutton.pack()
 
-playpausesong_btn = Button(root, text='Play/Pause Music')
-playpausesong_btn.pack()
+# playpausesong_btn = Button(root, text='Play/Pause Music')
+# playpausesong_btn.pack()
 
-# playsong_btn = Button(root, text='Play Music')
-# playsong_btn.pack()
+decreasevolume_btn = Button(root, text='Decrease Volume')
+decreasevolume_btn.pack()
+
+increasevolume_btn = Button(root, text='Increase Volume')
+increasevolume_btn.pack()
 
 nextbutton.bind("<Button-1>", nextsong)
 previousbutton.bind("<Button-1>", prevsong)
 stopbutton.bind("<Button-1>", stopsong)
-playpausesong_btn.bind("<Button-1>", playpausesong)
-# playsong_btn.bind("<Button-1>", playsong)
+decreasevolume_btn.bind("<Button-1>", decreasevolume)
+increasevolume_btn.bind("<Button-1>", increasevolume)
+# decreasevolume_btn.bind("<Button-1>", decreasevolume)
+
+# playpausesong_btn.bind("<Button-1>", playpausesong)
 
 songlabel.pack()
+
+server_address = ('0.0.0.0', 1234)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(server_address)
+server_socket.listen(500)
+thread = Thread(target=accept_conn)
+thread.start()
 
 root.mainloop()
